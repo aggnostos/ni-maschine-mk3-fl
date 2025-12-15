d8cc7e60135fe05125788eb640cd12d0eda1303f
Mark Rybin <aggnostos4@gmail.com>
2025-12-15 20:43:20 +0300

    add enums

---
+++ src/enums.py
@@ -0,0 +1,49 @@
+"""Enums for the Maschine MK3 FL Studio MIDI script"""
+
+from enum import Enum
+
+from colors import *
+
+
+__all__ = [
+    "PadMode",
+    "FourDEncoderMode",
+    "PluginColors",
+    "ChannelColors",
+]
+
+
+# -------- Plugin and channel colors for OMNI/PAD modes, feel free to change these with any others from the list --------
+class PluginColors(Enum):
+    """Plugin Colors"""
+
+    DEFAULT = Green1
+    HIGHLIGHTED = Green3
+
+
+class ChannelColors(Enum):
+    """Channel Colors"""
+
+    DEFAULT = White1
+    HIGHLIGHTED = White3
+
+
+# ------------------------------------------------------------------------------------------------------------------------
+
+
+class PadMode(Enum):
+    """Pad Modes"""
+
+    OMNI = 0
+    KEYBOARD = 1
+    CHORDS = 2
+    STEP = 3
+
+
+class FourDEncoderMode(Enum):
+    """4D Encoder Modes"""
+
+    JOG = 0
+    VOLUME = 1
+    SWING = 2
+    TEMPO = 3
