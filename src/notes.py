bd2e496ba20825f8ff0d14345f71adede59c8abc
Mark Rybin <aggnostos4@gmail.com>
2025-12-19 02:45:24 +0300

    update notes

--- src/notes.py
+++ src/notes.py
@@ -12,132 +12,132 @@ __all__ = [
 # DO NOT CHANGE THE NOTE VALUES BELOW, UNLESS YOU KNOW WHAT YOU ARE DOING!
 
 C1 = 0
-Cs1 = 1
+CS1 = 1
 D1 = 2
-Ds1 = 3
+DS1 = 3
 E1 = 4
 F1 = 5
-Fs1 = 6
+FS1 = 6
 G1 = 7
-Gs1 = 8
+GS1 = 8
 A1 = 9
-As1 = 10
+AS1 = 10
 B1 = 11
 C2 = 12
-Cs2 = 13
+CS2 = 13
 D2 = 14
-Ds2 = 15
+DS2 = 15
 E2 = 16
 F2 = 17
-Fs2 = 18
+FS2 = 18
 G2 = 19
-Gs2 = 20
+GS2 = 20
 A2 = 21
-As2 = 22
+AS2 = 22
 B2 = 23
 C3 = 24
-Cs3 = 25
+CS3 = 25
 D3 = 26
-Ds3 = 27
+DS3 = 27
 E3 = 28
 F3 = 29
-Fs3 = 30
+FS3 = 30
 G3 = 31
-Gs3 = 32
+GS3 = 32
 A3 = 33
-As3 = 34
+AS3 = 34
 B3 = 35
 C4 = 36
-Cs4 = 37
+CS4 = 37
 D4 = 38
-Ds4 = 39
+DS4 = 39
 E4 = 40
 F4 = 41
-Fs4 = 42
+FS4 = 42
 G4 = 43
-Gs4 = 44
+GS4 = 44
 A4 = 45
-As4 = 46
+AS4 = 46
 B4 = 47
 C5 = 48
-Cs5 = 49
+CS5 = 49
 D5 = 50
-Ds5 = 51
+DS5 = 51
 E5 = 52
 F5 = 53
-Fs5 = 54
+FS5 = 54
 G5 = 55
-Gs5 = 56
+GS5 = 56
 A5 = 57
-As5 = 58
+AS5 = 58
 B5 = 59
 C6 = 60
-Cs6 = 61
+CS6 = 61
 D6 = 62
-Ds6 = 63
+DS6 = 63
 E6 = 64
 F6 = 65
-Fs6 = 66
+FS6 = 66
 G6 = 67
-Gs6 = 68
+GS6 = 68
 A6 = 69
-As6 = 70
+AS6 = 70
 B6 = 71
 C7 = 72
-Cs7 = 73
+CS7 = 73
 D7 = 74
-Ds7 = 75
+DS7 = 75
 E7 = 76
 F7 = 77
-Fs7 = 78
+FS7 = 78
 G7 = 79
-Gs7 = 80
+GS7 = 80
 A7 = 81
-As7 = 82
+AS7 = 82
 B7 = 83
 C8 = 84
-Cs8 = 85
+CS8 = 85
 D8 = 86
-Ds8 = 87
+DS8 = 87
 E8 = 88
 F8 = 89
-Fs8 = 90
+FS8 = 90
 G8 = 91
-Gs8 = 92
+GS8 = 92
 A8 = 93
-As8 = 94
+AS8 = 94
 B8 = 95
 C9 = 96
-Cs9 = 97
+CS9 = 97
 D9 = 98
-Ds9 = 99
+DS9 = 99
 E9 = 100
 F9 = 101
-Fs9 = 102
+FS9 = 102
 G9 = 103
-Gs9 = 104
+GS9 = 104
 A9 = 105
-As9 = 106
+AS9 = 106
 B9 = 107
 C10 = 108
-Cs10 = 109
+CS10 = 109
 D10 = 110
-Ds10 = 111
+DS10 = 111
 E10 = 112
 F10 = 113
-Fs10 = 114
+FS10 = 114
 G10 = 115
-Gs10 = 116
+GS10 = 116
 A10 = 117
-As10 = 118
+AS10 = 118
 B10 = 119
 C11 = 120
-Cs11 = 121
+CS11 = 121
 D11 = 122
-Ds11 = 123
+DS11 = 123
 E11 = 124
 F11 = 125
-Fs11 = 126
+FS11 = 126
 G11 = 127
 
 ROOT_NOTE = C5
@@ -150,79 +150,79 @@ ROOT_NOTE = C5
 # JUST MAKE SURE THERE IS ALWAYS 16 CHORDS IN EACH CHORDSET!
 
 min1 = [  # MINOR 1
-    [C4, C5, Ds5, G5],  # 1
-    [Ds4, C5, Ds5, G5],
-    [F4, C4, F5, Gs5],
+    [C4, C5, DS5, G5],  # 1
+    [DS4, C5, DS5, G5],
+    [F4, C4, F5, GS5],
     [G3, B4, D5, G5],
-    [Gs3, C5, Ds5, G5],  # 5
-    [Ds4, As4, Ds5, G5],
-    [G3, As4, D5, G5],
-    [As3, As4, D5, F5],
+    [GS3, C5, DS5, G5],  # 5
+    [DS4, AS4, DS5, G5],
+    [G3, AS4, D5, G5],
+    [AS3, AS4, D5, F5],
     [F3, A4, C5, F5],  # 9
-    [Gs3, C5, F5, Gs5],
-    [G3, C5, Ds5, G5],
+    [GS3, C5, F5, GS5],
+    [G3, C5, DS5, G5],
     [G3, B4, D5, G5],
-    [F3, D4, F5, Gs5],  # 13
-    [D4, D5, F5, As5],
+    [F3, D4, F5, GS5],  # 13
+    [D4, D5, F5, AS5],
     [D4, C5, D5, G5],
     [C4, C5, F5, G5],
 ]
 
 min2 = [  # MINOR 2
-    [C4, G4, C5, Ds5],
-    [B3, G4, B4, Ds5],
-    [As3, G4, C5, Ds5],
+    [C4, G4, C5, DS5],
+    [B3, G4, B4, DS5],
+    [AS3, G4, C5, DS5],
     [G3, B4, D5, G5],
-    [Gs3, C5, Ds5, G5],
-    [Ds4, As4, Ds5, G5],
-    [G3, As4, D5, G5],
-    [As3, As4, D5, F5],
+    [GS3, C5, DS5, G5],
+    [DS4, AS4, DS5, G5],
+    [G3, AS4, D5, G5],
+    [AS3, AS4, D5, F5],
     [F3, A4, C5, F5],
-    [Gs3, C5, F5, Gs5],
-    [G3, C5, Ds5, G5],
+    [GS3, C5, F5, GS5],
+    [G3, C5, DS5, G5],
     [G3, B4, D5, G5],
-    [C4, C5, Ds5, G5],
-    [F3, D4, F5, Gs5],
-    [As3, D4, F5, As5],
-    [As3, D4, D5, G5],
+    [C4, C5, DS5, G5],
+    [F3, D4, F5, GS5],
+    [AS3, D4, F5, AS5],
+    [AS3, D4, D5, G5],
 ]
 
 min3 = [  # SYNTHWAVE
     [C4, G4, C5, D5, G5],
-    [C4, G4, As4, D5, F5],
+    [C4, G4, AS4, D5, F5],
     [D4, A4, C5, D5, F5],
     [D4, A5, C5, E5, G5],
     [E4, G4, C5, D5, G5],
-    [D4, G4, As4, D5, F5],
+    [D4, G4, AS4, D5, F5],
     [A3, A4, C5, D5, F5],
     [A3, A4, C5, E5, G5],
-    [Ds4, Ds5, Fs5, As5],
-    [Ds4, Cs5, F5, Gs5],
-    [Cs4, Cs5, F5, Gs5],
-    [Ds4, F5, Gs5, Cs6],
-    [Cs4, F5, Gs5, Cs6],
-    [C4, Ds5, Gs5, C6],
-    [C4, Ds5, G5, As5],
-    [As3, D4, G5, As5],
+    [DS4, DS5, FS5, AS5],
+    [DS4, CS5, F5, GS5],
+    [CS4, CS5, F5, GS5],
+    [DS4, F5, GS5, CS6],
+    [CS4, F5, GS5, CS6],
+    [C4, DS5, GS5, C6],
+    [C4, DS5, G5, AS5],
+    [AS3, D4, G5, AS5],
 ]
 
 min4 = [  # EPIC
-    [Cs3, D4, G4, As4],
+    [CS3, D4, G4, AS4],
     [F3, C4, F4, A4],
-    [G3, D4, G4, As4],
-    [As3, F4, As4, D5],
+    [G3, D4, G4, AS4],
+    [AS3, F4, AS4, D5],
     [D3, A3, D4, F4],
-    [C3, G3, C4, Ds4],
+    [C3, G3, C4, DS4],
     [F3, C4, F4, A4],
-    [G3, D4, G4, As4],
-    [C4, C5, Ds5, G5],
-    [C4, C5, Ds5, G5],
-    [C4, C5, Ds5, G5],
-    [C4, C5, Ds5, G5],
-    [C4, C5, Ds5, G5],
-    [C4, C5, Ds5, G5],
-    [C4, C5, Ds5, G5],
-    [C4, C5, Ds5, G5],
+    [G3, D4, G4, AS4],
+    [C4, C5, DS5, G5],
+    [C4, C5, DS5, G5],
+    [C4, C5, DS5, G5],
+    [C4, C5, DS5, G5],
+    [C4, C5, DS5, G5],
+    [C4, C5, DS5, G5],
+    [C4, C5, DS5, G5],
+    [C4, C5, DS5, G5],
 ]
 
 maj1 = [
@@ -311,14 +311,14 @@ CHORD_SETS = [min1, min2, min3, min4, maj1, maj2, maj3, maj4]
 # JUST MAKE SURE THAT THERE IS EXACTLY 16 NOTES IN EACH GROUP!
 
 # fmt: off
-scale_1 = [C4, D4, Gs4, As4, Cs4, E4, A4, F4, B4, Fs4, Ds4, G4, C5, Cs5, D5, G5]  # BATTERY
-scale_2 = [C5, D5, Ds5, F5, G5, Gs5, As5, C6, D6, Ds6, F6, G6, Gs6, As6, C7, D7]  # MINOR
+scale_1 = [C4, D4, GS4, AS4, CS4, E4, A4, F4, B4, FS4, DS4, G4, C5, CS5, D5, G5]  # BATTERY
+scale_2 = [C5, D5, DS5, F5, G5, GS5, AS5, C6, D6, DS6, F6, G6, GS6, AS6, C7, D7]  # MINOR
 scale_3 = [C5, D5, E5, F5, G5, A5, B5, C6, D6, E6, F6, G6, A6, B6, C7, D7]        # MAJOR
-scale_4 = [Cs4, C4, Fs4, As7, E4, D4, As4, Gs4, C5, B4, A4, G4, Cs5, G5, Ds5, F5] # FPC
-scale_5 = [C5, Cs5, D5, Ds5, E5, F5, Fs5, G5, Gs5, A5, As5, B5, C6, Cs6, D6, Ds6] # CHROMATIC
-scale_6 = [C5, Cs5, E5, F5, G5, Gs5, B5, C6, Cs6, E6, F6, G6, Gs6, B6, C7, Cs7]	  # ARABIC
-scale_7 = [C5, Cs5, D5, Ds5, E5, F5, Fs5, G5, Gs5, A5, As5, B5, C6, Cs6, D6, Ds6] # CUSTOM
-scale_8 = [C5, Cs5, D5, Ds5, E5, F5, Fs5, G5, Gs5, A5, As5, B5, C6, Cs6, D6, Ds6] # CUSTOM
+scale_4 = [CS4, C4, FS4, AS7, E4, D4, AS4, GS4, C5, B4, A4, G4, CS5, G5, DS5, F5] # FPC
+scale_5 = [C5, CS5, D5, DS5, E5, F5, FS5, G5, GS5, A5, AS5, B5, C6, CS6, D6, DS6] # CHROMATIC
+scale_6 = [C5, CS5, E5, F5, G5, GS5, B5, C6, CS6, E6, F6, G6, GS6, B6, C7, CS7]	  # ARABIC
+scale_7 = [C5, CS5, D5, DS5, E5, F5, FS5, G5, GS5, A5, AS5, B5, C6, CS6, D6, DS6] # CUSTOM
+scale_8 = [C5, CS5, D5, DS5, E5, F5, FS5, G5, GS5, A5, AS5, B5, C6, CS6, D6, DS6] # CUSTOM
 # fmt: on
 
 # THERE MUST BE EXACTLY 8 SCALES!
